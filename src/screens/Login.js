d66b12fcfc65b696f55ec9ab5c07ce727b2735f2
CristianoNunes <cristianonunes99@gmail.com>
2019-12-20 11:57:15 -0300

    Continuaçao tela de login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -3,42 +3,64 @@ import styled from 'styled-components/native';
 
 const Page = styled.SafeAreaView`
     flex: 1;
+    background-color:#7b70e6;
+    justify-content:center;
 `;
 
-const Quadro = styled.View`
+/*const Quadro = styled.View`
     align-self:flex-start;
     width: 100%;
     height: 250px;
     border-radius: 25px;
     background-color:#7b70e6;
-`;
+`;*/
 
 const AreaLogin = styled.View`
+    flex-direction: column;
     align-self: center;
-    border: 1px solid #000;
+    border: 1px solid #fff;
     border-radius: 20px;
     width: 270px;
     height: 250px;
     justify-content: center;
     align-items: center;
     background-color:#fff;
+    align-content:space-between;
 `;
 
 const Titulo = styled.Text`
+    padding-bottom: 20px;
     font-size: 20px;
 `;
 
-const Input = styled.TextInput`
+const InputLogin = styled.TextInput`
     font-size: 15px;
-    border: 1px solid #000;
+    border: 1px solid #eee;
     width: 200px;
     height: 50px;
     border-radius: 25px;
     padding: 15px;
     background-color:#eee;
-`
+`;
+
+const InputSenha = styled.TextInput`
+    padding-top:10px;
+    font-size: 15px;
+    border: 1px solid #eee;
+    width: 200px;
+    height: 50px;
+    border-radius: 25px;
+    padding: 15px;
+    background-color:#eee;
+`;
 
 const Botao = styled.Button`
+`;
+
+const Separator = styled.View`
+    marginVertical: 8px;
+    borderBottomColor: #737373;
+    borderBottomWidth: 100px;
 `;
 
 const Screen = (props) => {
@@ -51,12 +73,12 @@ const Screen = (props) => {
 
     return (
         <Page>
-            <Quadro></Quadro>
             <AreaLogin>
                 <Titulo>Bem vindo ao Personal Fit</Titulo>
-                <Input placeholder='Login' value={nome} onChangeText={e=>setNome(e)} />
-                <Input placeholder='Senha'/>
+                <InputLogin placeholder='Login' value={nome} onChangeText={e=>setNome(e)} />
+                <InputSenha placeholder='Senha'/>
                 <Botao title='Entrar' onPress={fazerLogin} />
+
             </AreaLogin>
         </Page>
 
